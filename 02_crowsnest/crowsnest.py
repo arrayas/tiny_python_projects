3fece851f056183ea591e6fe101be2046a47526a
arrayas <manuel.arrayas@gmail.com>
2021-05-15 21:15:56 +0200

    Note this is set in .gitignore, so needs git add -f new file:   crowsnest.py

---
+++ 02_crowsnest/crowsnest.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Author : arrayas <arrayas@localhost>
+Date   : 2021-05-15
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('word',
+                        metavar='word',
+                        help='A word')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    word = args.word
+    article = "an" if word[0].lower() in "aeiou" else "a"
+    #print('Ahoy, Captain, {} {} off the larboard bow!' .format(article, word))
+    print(f'Ahoy, Captain, {article} {word} off the larboard bow!')
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
